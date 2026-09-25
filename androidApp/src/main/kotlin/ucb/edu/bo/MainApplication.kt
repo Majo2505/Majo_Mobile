b9cf7970d53ff0fd0a2529817013afa63ee44bae
Your Name <you@example.com>
2026-09-25 11:25:13 -0400

    x

---
+++ androidApp/src/main/kotlin/ucb/edu/bo/MainApplication.kt
@@ -0,0 +1,11 @@
+﻿package ucb.edu.bo
+
+import android.app.Application
+import ucb.edu.bo.di.initKoinAndroid
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKoinAndroid(this)
+    }
+}
---
+++ shared/src/androidMain/kotlin/ucb/edu/bo/di/KoinModules.android.kt
@@ -0,0 +1,15 @@
+﻿package ucb.edu.bo.di
+
+import android.content.Context
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import sharedModules
+
+fun initKoinAndroid(context: Context) {
+    startKoin {
+        androidContext(context)
+        androidLogger()
+        modules(sharedModules())
+    }
+}
